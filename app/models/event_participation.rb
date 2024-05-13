68fd6cb8b80d6a4ed91dfe3b8207437830bff5da
Abdul-Rahman0 <abdurhmankhan51@gmail.com>
2024-05-13 18:00:04 +0500

    add active admin for super admin

--- app/models/event_participation.rb
+++ app/models/event_participation.rb
@@ -1,4 +1,7 @@
 class EventParticipation < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "event_id", "id", "updated_at", "user_id"]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        #  enum role: [:user, :super_admin]
+
+        #  def admin_user?
+        #    super_admin?
+        #  end
 end
